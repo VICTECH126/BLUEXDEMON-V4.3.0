dcf7b5d314b7fa3f3e8d9ffe77c9a43268f129da
VICTECH126 <lawalv44@gmail.com>
2025-01-12 20:33:46 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 //Input number for pair code
-global.pairingNumber = "2348087531461"
+global.pairingNumber = "2348055110357"
 
 
 
